3f3652acdca89bf4ab59622032e03467e9f22ae8
Alex Filatov <alex@alexfilatov.com>
2022-03-28 18:20:02 +0100

    init

---
+++ lib/ueberauth_near.ex
@@ -0,0 +1,18 @@
+defmodule UeberauthNear do
+  @moduledoc """
+  Documentation for `UeberauthNear`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UeberauthNear.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ueberauth_near_test.exs
@@ -0,0 +1,8 @@
+defmodule UeberauthNearTest do
+  use ExUnit.Case
+  doctest UeberauthNear
+
+  test "greets the world" do
+    assert UeberauthNear.hello() == :world
+  end
+end
